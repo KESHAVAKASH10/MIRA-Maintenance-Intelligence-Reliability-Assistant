d9d8651c4849fea1f6c2b7f16f6f7a564e5e2e7f
KESHAVAKASH10 <keshavakash710@gmail.com>
2026-07-22 19:23:42 +0530

    Built Document Intelligence Page

--- frontend/src/pages/Documents.jsx
+++ frontend/src/pages/Documents.jsx
@@ -1,5 +1,143 @@
+import { useEffect, useState } from "react";
+
+import DocumentCard from "../components/DocumentCard";
+
+import {
+
+    getDocuments,
+
+    searchDocuments
+
+}
+
+    from "../services/documentService";
+
 function Documents() {
-    return <h1 className="text-3xl font-bold">Documents</h1>;
+
+    const [documents, setDocuments] = useState([]);
+
+    const [loading, setLoading] = useState(true);
+
+    const [query, setQuery] = useState("");
+
+    useEffect(() => {
+
+        loadDocuments();
+
+    }, []);
+
+    async function loadDocuments() {
+
+        try {
+
+            const data = await getDocuments();
+
+            setDocuments(data);
+
+        }
+
+        catch (error) {
+
+            console.log(error);
+
+        }
+
+        finally {
+
+            setLoading(false);
+
+        }
+
+    }
+
+    async function handleSearch(e) {
+
+        const value = e.target.value;
+
+        setQuery(value);
+
+        if (value === "") {
+
+            loadDocuments();
+
+            return;
+
+        }
+
+        const data = await searchDocuments(value);
+
+        setDocuments(data);
+
+    }
+
+    return (
+
+        <div className="p-8">
+
+            <h1 className="text-4xl font-bold mb-6">
+
+                Industrial Documents
+
+            </h1>
+
+            <input
+
+                value={query}
+
+                onChange={handleSearch}
+
+                placeholder="Search by filename, equipment or type..."
+
+                className="border rounded-lg p-3 w-full mb-8"
+
+            />
+
+            {
+
+                loading ?
+
+                    (
+
+                        <h2>
+
+                            Loading...
+
+                        </h2>
+
+                    )
+
+                    :
+
+                    (
+
+                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+
+                            {
+
+                                documents.map((doc, index) => (
+
+                                    <DocumentCard
+
+                                        key={index}
+
+                                        document={doc}
+
+                                    />
+
+                                ))
+
+                            }
+
+                        </div>
+
+                    )
+
+            }
+
+        </div>
+
+    );
+
 }
 
 export default Documents;
